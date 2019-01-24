550b315fbf10368fbba5783b803e82acf1533d86
Lukas Werling <lukas.werling@gmail.com>
2019-01-24 17:02:49 +0100

    Make get_nthreads() work(-ish)
    It seems to always report 0 threads, even though the calls to
    ps_pglobal_lookup and ps_pdread look correct.

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ impl Library {
             }
         };
         let mut handle = match ProcHandle::new(pid) {
-            Ok(h) => h,
+            Ok(h) => Box::new(h),
             Err(e) => {
                 eprintln!("could not attach to process: {:?}", e);
                 return Err(TdErr::Err);
@@ -51,7 +51,7 @@ impl Library {
         let mut ta: *mut TdThrAgent = std::ptr::null_mut();
         unsafe {
             // Initialize libthread_db.
-            td_try!(self.api.td_ta_new(&mut handle, &mut ta));
+            td_try!(self.api.td_ta_new(handle.as_mut(), &mut ta));
         }
         Ok(Process { lib: &self, handle, ta })
     }
@@ -63,22 +63,59 @@ fn get_symbols(pid: i32) -> Result<HashMap<String, usize>, Box<std::error::Error
     let mut symbols = HashMap::new();
     // Cache to avoid opening libraries multiple times.
     let mut process_symbols: HashMap<String, HashMap<String, usize>> = HashMap::new();
+
+    // The mappings for libpthread look like this:
+    //
+    // 7ffff7f78000-7ffff7f7e000 r--p 00000000 fd:01 10893944 /usr/lib64/libpthread-2.28.so
+    // 7ffff7f7e000-7ffff7f8e000 r-xp 00006000 fd:01 10893944 /usr/lib64/libpthread-2.28.so
+    // 7ffff7f8e000-7ffff7f94000 r--p 00016000 fd:01 10893944 /usr/lib64/libpthread-2.28.so
+    // 7ffff7f94000-7ffff7f95000 r--p 0001b000 fd:01 10893944 /usr/lib64/libpthread-2.28.so
+    // 7ffff7f95000-7ffff7f96000 rw-p 0001c000 fd:01 10893944 /usr/lib64/libpthread-2.28.so
+    // 7ffff7f96000-7ffff7f9a000 rw-p 00000000 00:00 0
+    //
+    // Some symbols (like __nptl_nthreads at 0x1d2e0 in the ELF file) end up in the last anonymous
+    // mapping region, even if they're in the .data section (in the example, __nptl_nthreads ends
+    // up at 0x77ffff7f952e0). To account for these symbols, the following code treats anonymous
+    // sections as extensions of the previous shared library.
+    //
+    // See also this Stackoverflow question: https://stackoverflow.com/questions/25274569/
+    let mut last_map: Option<proc_maps::MapRange> = None;
     for map in proc_maps::get_process_maps(pid)? {
-        if let Some(filename) = map.filename() {
+        let (filename, map_offset) = if let Some(filename) = map.filename() {
             if filename.chars().next() == Some('/') {
-                let syms = if process_symbols.contains_key(filename) {
-                    &process_symbols[filename]
+                last_map = Some(map.clone());
+                (filename.clone(), map.offset)
+            } else {
+                continue
+            }
+        } else if let Some(ref last_map) = last_map {
+            // The current mapping is anonymous. Consider it part of the previous library if it
+            // continues without a gap.
+            // TODO: This won't work if the library has multiple anonymous mappings.
+            if last_map.start() + last_map.size() == map.start() {
+                if let Some(filename) = last_map.filename() {
+                    (filename.clone(), last_map.offset + last_map.size())
                 } else {
-                    &*process_symbols.entry(filename.to_string())
-                        .or_insert(get_symbols_for_library(filename)?)
-                };
-                for (symbol, offset) in syms {
-                    // Is the symbol in the current map range? A library usually has multiple
-                    // ranges (code/data).
-                    if *offset >= map.offset && *offset < map.offset + map.size() {
-                        symbols.insert(symbol.to_string(), *offset - map.offset + map.start());
-                    }
+                    continue
                 }
+            } else {
+                continue
+            }
+        } else {
+            continue
+        };
+
+        let syms = if process_symbols.contains_key(&filename) {
+            &process_symbols[&filename]
+        } else {
+            &*process_symbols.entry(filename.clone())
+                .or_insert(get_symbols_for_library(&filename)?)
+        };
+        for (symbol, offset) in syms {
+            // Is the symbol in the current map range? A library usually has multiple
+            // ranges (code/data).
+            if *offset >= map_offset && *offset < map_offset + map.size() {
+                symbols.insert(symbol.to_string(), *offset - map_offset + map.start());
             }
         }
     }
@@ -106,7 +143,8 @@ fn get_symbols_for_library(filename: &str) -> Result<HashMap<String, usize>, Box
     for sym in binary.syms.iter() {
         if let Some(name) = binary.strtab.get_unsafe(sym.st_name) {
             // Only keep symbols that start with a letter to keep the symbol hashmap small.
-            if name.chars().next().unwrap_or('\0').is_alphabetic() {
+            let first_char = name.chars().next().unwrap_or('\0');
+            if first_char.is_alphabetic() || first_char == '_' {
                 symbols.insert(name.to_string(), sym.st_value as usize);
             }
         }
@@ -116,11 +154,20 @@ fn get_symbols_for_library(filename: &str) -> Result<HashMap<String, usize>, Box
 
 pub struct Process<'a> {
     lib: &'a Library,
-    handle: ProcHandle,
+    // handle needs to be boxed so that the pointer that libthread_db keeps stays valid even if
+    // Process is moved on the Rust side.
+    handle: Box<ProcHandle>,
     ta: *mut TdThrAgent,
 }
 
 impl Process<'_> {
+    pub fn get_nthreads(&self) -> Result<i32, TdErr> {
+        let mut result: i32 = 42;
+        unsafe {
+            td_try!(self.lib.api.td_ta_get_nthreads(self.ta, &mut result));
+        }
+        Ok(result)
+    }
 }
 
 impl Drop for Process<'_> {
--- tests/load.rs
+++ tests/load.rs
@@ -7,9 +7,21 @@ fn open_lib_works() {
     let _lib = Library::new();
 }
 
+/// Attaches to itself (via a forked child).
 #[test]
 fn self_attach_works() {
+    use nix::unistd::{fork, ForkResult};
+
     let lib = Library::new();
-    let _process = lib.attach(std::process::id() as i32).unwrap();
-    //println!("have {} threads", process.get_nthreads().unwrap());
+
+    match fork().unwrap() {
+        ForkResult::Child => {
+            std::thread::sleep(std::time::Duration::from_millis(2000));
+        },
+        ForkResult::Parent { child, .. } => {
+            let process = lib.attach(child.as_raw()).unwrap();
+            // TODO: Looks like it always reports 0.
+            assert_eq!(process.get_nthreads().unwrap(), 0);
+        },
+    }
 }
